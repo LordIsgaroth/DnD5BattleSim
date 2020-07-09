98d2951e46635cd6592fdf1f867cfc85b74e532e
Isgaroth <lordisgaroth@gmail.com>
2020-07-09 09:46:20 +0300

    Tile selection reworked through raycast

--- Assets/Scripts/TurnController.cs
+++ Assets/Scripts/TurnController.cs
@@ -5,41 +5,41 @@ using UnityEngine.Tilemaps;
 
 public class TurnController : MonoBehaviour
 {
-    public GameObject currentCharacter;
-    public Grid grid;
-    public Tilemap UILayer;
-    public Tile tileSelected;
-        
+    [SerializeField] private GameObject currentCharacter;
+    [SerializeField] private Grid grid;
+    [SerializeField] private Tilemap UILayer;
+    [SerializeField] private Camera camera;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Canvas userInterface;
+    [SerializeField] private Tile tileSelected;
+
     void Start()
     {
-        UILayer.SetTile(new Vector3Int(0, 0, -1), tileSelected);
+        //UILayer.SetTile(new Vector3Int(0, 0, -1), tileSelected);
     }
 
-    Vector3Int previousCoordinates;
+    Vector3Int prevCoordinates;
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3Int mousePosition = Vector3Int.CeilToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        Vector3 mousePosition1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
 
-        Vector3Int tileCoordinates = UILayer.WorldToCell(new Vector3(mousePosition.x - 1, mousePosition.y - 1, mousePosition.z));
-        Vector3 tileCoordinates1 = UILayer.WorldToLocal(mousePosition1);
-
-        UILayer.SetTile(tileCoordinates, tileSelected);
-
-        if (previousCoordinates != tileCoordinates)
+        if (hit.collider != null)
         {
-            UILayer.SetTile(previousCoordinates, null);
+            Vector3 mouseWorldPos = camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int tilemapMousePos = UILayer.WorldToCell(mouseWorldPos);
+            
+            if (UILayer.GetTile(tilemapMousePos) == null)
+            {
+                UILayer.SetTile(tilemapMousePos, tileSelected);
+            }
+
+            if (tilemapMousePos != prevCoordinates)
+            {
+                UILayer.SetTile(prevCoordinates, null);
+            }
+
+            prevCoordinates = tilemapMousePos;
         }
-        previousCoordinates = tileCoordinates;
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            currentCharacter.transform.position = tileCoordinates;
-            Debug.Log("!");
-            Debug.Log(currentCharacter.transform.position);
-            Debug.Log("!");
-        }            
     }
 }
