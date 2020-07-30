10f3ac9ddd0b6abbf55c58276c368361db5f3f72
Isgaroth <lordisgaroth@gmail.com>
2020-07-30 07:42:14 +0300

    Class for movement added

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Numerics;
+using UnityEngine;
+
+public static class CharacterMovement
+{
+    public static void getAvalibleNodes(Vector3Int position, int speed)
+    {
+        Hashtable avalibleNodes = new Hashtable();
+
+        //avalibleNodes.Add();
+
+        //return avalibleNodes;
+    }
+}
