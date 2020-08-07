e5db8073b8d5031e643fb68d1f8794ddd8b5aef9
LordIsgaroth <lordisgaroth@gmail.com>
2020-08-07 16:15:52 +0300

    Script for camera control added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+
+    private Vector3 MousePosition;
+    private float angle;
+    public float sensitivity = 1F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MousePosition = Input.mousePosition;
+    }
+
+    void FixedUpdate()
+    {
+        /*angle = sensitivity * ((MousePosition.x - (Screen.width / 2)) / Screen.width);
+        camera.transform.RotateAround(camera.transform.position, camera.transform.up, angle);
+        angle = sensitivity * ((MousePosition.y - (Screen.height / 2)) / Screen.height);
+        camera.transform.RotateAround(camera.transform.position, camera.transform.right, -angle);*/
+    }
+}
