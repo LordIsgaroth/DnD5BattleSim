dcb2db9355bb4898176c57edc0fdd8d8b60e3f89
LordIsgaroth <lordisgaroth@gmail.com>
2020-08-19 17:42:47 +0300

    Damage and Range properties added to Weapon class

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New weapon", menuName = "Weapon", order = 51)]
 public class Weapon : Equipment
 {
+    [SerializeField] DiceSet damage;
+    [SerializeField] int range;
+
     public Weapon(string name, int weight, int value, EquipmentType type) : base(name, weight, value, type)
     {        
     }
