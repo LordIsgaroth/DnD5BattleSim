0d675f7e42a4b7d40ca0473638ed49ed2bfed6d5
LordIsgaroth <lordisgaroth@gmail.com>
2020-08-19 17:27:10 +0300

    Dice set class added

---
+++ Assets/Scripts/DiceSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New dice set", menuName = "Dice set", order = 51)]
+public class DiceSet : ScriptableObject
+{
+    [SerializeField] int quantity;
+    [SerializeField] int type;
+}
