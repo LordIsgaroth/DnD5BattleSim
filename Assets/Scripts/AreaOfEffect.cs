ed6bb3638dbead63e61bf5de002b90cca26108ce
LordIsgaroth <lordisgaroth@gmail.com>
2020-08-31 17:45:59 +0300

    Area of Effect class created

---
+++ Assets/Scripts/AreaOfEffect.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaOfEffect
+{
+    private HashSet<Vector3Int> tiles = new HashSet<Vector3Int>();
+    private List<Character> characters = new List<Character>();
+}
