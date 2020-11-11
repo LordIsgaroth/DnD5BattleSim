9afa60e690fc1aca33dabe1be615725b342b4333
Isgaroth <lordisgaroth@gmail.com>
2020-11-11 16:19:47 +0300

    Auto scrolling of log window

--- Assets/Scripts/LogWindow.cs
+++ Assets/Scripts/LogWindow.cs
@@ -6,10 +6,27 @@ using UnityEngine.UI;
 public class LogWindow : MonoBehaviour
 {
     [SerializeField] private Text logText;
+    [SerializeField] private ScrollRect scroll;
+
+    bool scrollingDown;
 
     public void DisplayIntoLogWindow(string message)
     {
-
         logText.text += message + '\n';
+        scroll.normalizedPosition = new Vector2 (0,0);
+        scrollingDown = true;
+    }
+
+    private void Update()
+    {
+        if (scrollingDown)
+        {
+            scroll.normalizedPosition = Vector2.Lerp(scroll.normalizedPosition, new Vector2(0,0), 1f);
+        }
+
+        if (scroll.normalizedPosition.y == 0)
+        {
+            scrollingDown = false;
+        }
     }
 }
